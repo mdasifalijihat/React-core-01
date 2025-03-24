c7b902ef250e432838de34256b9260691c56254e
mdasifalijihat <mdasifalijihat@gmail.com>
2025-03-24 15:56:21 +0600

    add data

---
+++ src/Summary.jsx
@@ -0,0 +1,24 @@
+/***
+ * 
+ * Components
+ * JSX
+ * Props
+ * State 
+ * Events 
+ * [ Conditional Rendering]
+ * 
+ * Server site data load...
+ * 
+ *  */
+
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+// const loadData = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = res.json();
+//     console.log(data)
+//     return data;
+// }
